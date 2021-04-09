556381f64893f28a163f790b71fbe701c39e03cf
Ekimaru <74165503+Ekimaru@users.noreply.github.com>
2021-04-09 08:25:41 -0500

    Cambios Ejemplo LocalStorage

--- src/components/LocalStorage.jsx
+++ src/components/LocalStorage.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 import { useLocalStorage } from '../hooks/useLocalStorage';
-// import { useLocalStorage } from '../hooks/useLocalStorage';
 
 export default function LocalStorage() {
-    const [text, setText] = useLocalStorage('text', '')
-    const [secondText, setScondText] = useLocalStorage('secondText', '')
+    const [text, setText] = useLocalStorage("text", "");
+    const [text_2, setText_2] = useLocalStorage("text_2", "")
+
     return (
         <>
             <textarea
@@ -13,8 +13,8 @@ export default function LocalStorage() {
                 placeholder="Texto Prueba">
             </textarea>
             <textarea
-                onChange={({ target }) => setScondText(target.value)}
-                value={secondText}
+                onChange={({ target }) => setText_2(target.value)}
+                value={text_2}
                 placeholder="Texto Prueba 2">
             </textarea>
         </>
