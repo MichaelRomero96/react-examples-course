3c98c730a6b6e344262fef3cd07e80f94e728e1c
Ekimaru <74165503+Ekimaru@users.noreply.github.com>
2021-04-12 09:50:13 -0500

    Clase ContextAPI useReducer

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,36 +1,20 @@
-import axios from 'axios'
-import React, { useState, useEffect } from 'react'
-import { obtenerDatosAxios } from '../services/users';
+import { useEffect } from "react";
+import { obtenerDatosAxios } from "../services/users";
 
-export default function Users() {
+export default function Users({ setUsers, ...datos }) {
 
-    const [users, setUsers] = useState([]);
-
-    const obtenerDatos = async () => {
-        const data = await obtenerDatosAxios();
-        setUsers(data)
+    const getUsers = async () => {
+        const response = await obtenerDatosAxios();
+        setUsers(response);
     }
+
     useEffect(() => {
-        obtenerDatos();
+        getUsers();
     }, []);
-    useEffect(() => {
-        console.log(users)
-    }, [users])
+
     return (
         <>
-            <h1>Desde Users</h1>
-            { users.length ?
-                users.map(user => (
-                    <>
-                        <li key={user.id}>
-                            {user.name}
-                        </li>
-                        <ul>{user.email}</ul>
-                        <ul>{user.username}</ul>
-                        <ul>{user.body}</ul>
-                    </>
-                ))
-                : null}
+            <h1>Users</h1>
         </>
     )
 }
