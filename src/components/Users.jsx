242aca436463833472ea1568522b891b81d90056
Ekimaru <74165503+Ekimaru@users.noreply.github.com>
2021-04-13 10:11:29 -0500

    Ejercicio Validación Login

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,12 +1,23 @@
 import { useEffect } from "react";
+import { useDispatch, useStore } from "../context/provider/UsersProvider";
+import { types } from "../context/reducers/UsersReducer";
 import { obtenerDatosAxios } from "../services/users";
 
-export default function Users({ setUsers, ...datos }) {
+export default function Users() {
+    const { users } = useStore();
+    const dispatch = useDispatch();
 
     const getUsers = async () => {
-        const response = await obtenerDatosAxios();
-        setUsers(response);
+        const response = await fetch('https://jsonplaceholder.typicode.com/users')
+        const data = await response.json()
+        dispatch({
+            type: types.setUsers,
+            payload: data
+        })
     }
+    useEffect(() => {
+        console.log(users)
+    }, [users])
 
     useEffect(() => {
         getUsers();
@@ -15,6 +26,9 @@ export default function Users({ setUsers, ...datos }) {
     return (
         <>
             <h1>Users</h1>
+            {users.map(user => (
+                <li key={user.id}>{user.name}</li>
+            ))}
         </>
     )
 }
